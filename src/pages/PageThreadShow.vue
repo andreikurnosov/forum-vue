74ede68ec13e6c88bdf7da941f210a10199865b4
nixdie <nixdie@yandex.ru>
2019-01-22 12:58:26 +0700

    repliesCount and contributorsCount functions

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -14,7 +14,7 @@
 
       <p>
         By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt"/>
-        <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies from 3 contributors</span>
+        <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies from {{contributorsCount}} contributors</span>
       </p>
       <PostList :posts="posts"/>
       <PostEditor
@@ -42,6 +42,25 @@ export default {
     thread () {
       return this.$store.state.threads[this.id]
     },
+
+    contributorsCount () {
+      // find the replies
+      const replies = Object.keys(this.thread.posts)
+        .filter(postId => postId !== this.thread.firstPostId)
+        .map(postId => this.$store.state.posts[postId])
+
+      // get the user ids
+      const userIds = replies.map(post => post.userId)
+
+      // count the unique ids
+
+      return userIds.filter((item, index) => index === userIds.indexOf(item)).length
+    },
+
+    repliesCount () {
+      return this.$store.getters.threadRepliesCount(this.thread['.key'])
+    },
+
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts)
