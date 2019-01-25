11ac40d9203f8f0b4e021dd23a91aa637e79ee67
nixdie <nixdie@yandex.ru>
2019-01-26 02:38:25 +0700

    added npm script for seed fb, .env in webpack template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,19 @@
 'use strict'
+
+require('dotenv').config()
+
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: JSON.stringify(process.env.FIREBASE_NODE_ENV),
+
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+
+  FIREBASE_STORAGE_BUKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUKET),
+
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID)
 }
--- src/main.js
+++ src/main.js
@@ -15,12 +15,12 @@ Vue.config.productionTip = false
 // Initialize Firebase
 
 const config = {
-  apiKey: 'AIzaSyAHmHFt5iyCIl59kVlgF2qW6pfe3FlT6cE',
-  authDomain: 'forum-vue.firebaseapp.com',
-  databaseURL: 'https://forum-vue.firebaseio.com',
-  projectId: 'forum-vue',
-  storageBucket: 'forum-vue.appspot.com',
-  messagingSenderId: '335656754127'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
 }
 
 firebase.initializeApp(config)
