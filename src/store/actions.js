8b7f5aaa821da973054ae34086226282b7f583aa
nixdie <nixdie@yandex.ru>
2019-01-25 22:43:41 +0700

    store new threads in fb

--- src/store/actions.js
+++ src/store/actions.js
@@ -26,41 +26,6 @@ export default {
       })
   },
 
-  updateThread ({state, commit, dispatch}, {title, text, id}) {
-    return new Promise((resolve, reject) => {
-      const thread = state.threads[id]
-      const newThread = {...thread, title}
-
-      commit('setThread', {thread: newThread, threadId: id})
-      dispatch('updatePost', {id: thread.firstPostId, text})
-        .then(() => {
-          resolve(newThread)
-        })
-    })
-  },
-
-  updatePost ({state, commit}, {id, text}) {
-    return new Promise((resolve, reject) => {
-      const post = state.posts[id]
-      commit('setPost', {
-        postId: id,
-        post: {
-          ...post,
-          text,
-          edited: {
-            at: Math.floor(Date.now() / 1000),
-            by: state.authId
-          }
-        }
-      })
-      resolve(post)
-    })
-  },
-
-  updateUser ({commit}, user) {
-    commit('setUser', {userId: user['.key'], user})
-  },
-
   fetchCategory: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'categories', id, emoji: 'category'}),
 
   fetchForum: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'forums', id, emoji: 'forum'}),
@@ -112,32 +77,86 @@ export default {
 
   createThread ({state, commit, dispatch}, {text, title, forumId}) {
     return new Promise((resolve, reject) => {
-      const threadId = 'greatPost' + Math.random()
+      const threadId = firebase.database().ref('threads').push().key
+
+      const postId = firebase.database().ref('posts').push().key
 
       const userId = state.authId
 
       const publishedAt = Math.floor(Date.now() / 1000)
 
-      const thread = {
-        '.key': threadId,
-        title,
-        forumId,
-        publishedAt,
-        userId
-      }
+      const thread = {title, forumId, publishedAt, userId, firstPostId: postId, posts: {}}
 
-      commit('setThread', {threadId, thread})
+      thread.posts[postId] = postId
 
-      commit('appendThreadToUser', {parentId: userId, childId: threadId})
+      const post = {text, publishedAt, threadId, userId}
 
-      commit('appendThreadToForum', {parentId: forumId, childId: threadId})
+      const updates = {}
+      updates[`threads/${threadId}`] = thread
 
-      dispatch('createPost', {text, threadId})
-        .then(post => {
-          commit('setThread', {threadId, thread: {...thread, firstPostId: post['.key']}})
+      updates[`forums/${forumId}/threads/${threadId}`] = threadId
+
+      updates[`users/${userId}/threads/${threadId}`] = threadId
+
+      updates[`posts/${postId}`] = post
+
+      updates[`users/${userId}/posts/${postId}`] = postId
+
+      firebase.database().ref().update(updates)
+        .then(() => {
+          // update thread
+
+          commit('setItem', {resource: 'threads', id: threadId, item: thread})
+
+          commit('appendThreadToForum', {parentId: forumId, childId: threadId})
+
+          commit('appendThreadToUser', {parentId: userId, childId: threadId})
+
+          // update post
+
+          commit('setItem', {resource: 'posts', item: post, id: postId})
+
+          commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+
+          commit('appendPostToUser', {parentId: post.userId, childId: postId})
+
+          resolve(state.threads[threadId])
         })
-
-      resolve(state.threads[threadId])
     })
+  },
+
+  updateThread ({state, commit, dispatch}, {title, text, id}) {
+    return new Promise((resolve, reject) => {
+      const thread = state.threads[id]
+      const newThread = {...thread, title}
+
+      commit('setThread', {thread: newThread, threadId: id})
+      dispatch('updatePost', {id: thread.firstPostId, text})
+        .then(() => {
+          resolve(newThread)
+        })
+    })
+  },
+
+  updatePost ({state, commit}, {id, text}) {
+    return new Promise((resolve, reject) => {
+      const post = state.posts[id]
+      commit('setPost', {
+        postId: id,
+        post: {
+          ...post,
+          text,
+          edited: {
+            at: Math.floor(Date.now() / 1000),
+            by: state.authId
+          }
+        }
+      })
+      resolve(post)
+    })
+  },
+
+  updateUser ({commit}, user) {
+    commit('setUser', {userId: user['.key'], user})
   }
 }
